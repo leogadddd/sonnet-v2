6f8493a87658e8ad6cb9143abc9090b403203fcc
leogadddd <leogadil04@gmail.com>
2025-03-24 23:25:53 +0800

    fix flickering if two tabs opened

--- components/settings-initializer.tsx
+++ components/settings-initializer.tsx
@@ -3,14 +3,17 @@
 import React, { useEffect } from "react";
 
 import { useTheme } from "next-themes";
+import { usePathname } from "next/navigation";
 
 import { useSettings } from "@/lib/store/use-settings";
 
 const SettingsInitializer = () => {
+  const pathname = usePathname();
   const { theme, setTheme } = useTheme();
   const { settings } = useSettings();
 
   useEffect(() => {
+    if (!pathname.includes("/app")) return;
     if (theme !== (settings.darkmode ? "dark" : "light"))
       setTheme(settings.darkmode ? "dark" : "light");
   }, [settings.darkmode, theme, setTheme]);
